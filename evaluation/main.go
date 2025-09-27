4b366fb9b1ad37f2781d9f34b45849f54501c63d
Jakub <pecet3107@gmail.com>
2025-09-27 15:36:36 +0200

    nice repl

--- evaluation/main.go
+++ evaluation/main.go
@@ -5,6 +5,12 @@ import (
 	"github.com/pecet3/hmbk-script/object"
 )
 
+var (
+	TRUE  = &object.Bool{Value: true}
+	FALSE = &object.Bool{Value: false}
+	NULL  = &object.Null{}
+)
+
 func Eval(n ast.Node) object.Object {
 	switch node := n.(type) {
 	case *ast.Program:
@@ -13,7 +19,10 @@ func Eval(n ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
-
+	case *ast.PrefixExpression:
+		return evalPrefixExpression(node.Operator, Eval(node.Right))
+	case *ast.Boolean:
+		return boolToBoolObj(node.Value)
 	}
 	return nil
 }
@@ -25,4 +34,45 @@ func evalStatement(stmts []ast.Statement) object.Object {
 		result = Eval(stmt)
 	}
 	return result
+}
+
+func boolToBoolObj(input bool) object.Object {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusExpression(right)
+	default:
+		return NULL
+	}
+
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
